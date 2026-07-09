24c0c9bbe44dae712135f7d1af93c656317d4eff
ianchang <iy2chang@gmail.com>
2026-07-09 17:17:28 +0900

    remove unused imports

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,8 @@
-import useGames, { type Platform } from "@/hooks/useGames";
+import useGames from "@/hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import type { Genre } from "@/hooks/useGenre";
 import type { GameQuery } from "@/App";
 
 interface Props {
