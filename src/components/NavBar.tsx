c47b5e52bcf4b63babffcacbaeddd4c1ae579cd6
ianchang <iy2chang@gmail.com>
2026-07-14 18:26:34 +0900

    build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <SearchInput />
     </HStack>
   );
 };
